91250320109a1f7d0d54d279af9b9c3d47e7b00e
hut <changchunstep.gmail.com>
2024-10-07 23:27:24 +0800

    update code

--- Thonny-projects/mqtt-temp-publish.py
+++ Thonny-projects/mqtt-temp-publish.py
@@ -9,11 +9,19 @@ import json
 from math import sin
 from umqtt.simple import MQTTClient
 
+from machine import Pin
 from dht_sensor import readDHT
+led_pin = Pin("LED", Pin.OUT)
 
-# Fill in your WiFi network name (ssid) and password here:
+
 wifi_ssid = "******"
 wifi_password = "******"
+
+# 日志相关
+log_file = open("_log.txt", "w")
+startTime_str = "the start time：%s" %str(time.localtime())
+log_file.write(startTime_str + "\n")
+log_file.flush()
 
 # Connect to WiFi
 wlan = network.WLAN(network.STA_IF)
@@ -25,6 +33,8 @@ while wlan.isconnected() == False:
     time.sleep(1)
 print("Connected to WiFi")
 
+led_pin.on()
+
 # Fill in your Adafruit IO Authentication and Feed MQTT Topic details
 mqtt_host = "192.168.31.11"
 mqtt_port = 1883
@@ -34,35 +44,68 @@ mqtt_publish_topic = "/fridge/temp-humidity"  # The MQTT topic for your Adafruit
 # It needs to be globally unique across all of Adafruit IO.
 mqtt_client_id = "WCwsVCBZa1xcSlRTUzwsaXkiUXlwOVVgKg"
 
-# Initialize our MQTTClient and connect to the MQTT server
-mqtt_client = MQTTClient(
-        client_id=mqtt_client_id,
-        server=mqtt_host,
-        port=mqtt_port)
-
-mqtt_client.connect()
-
-
-counter = 0
-try:
-    while True:
-        temp_hum = readDHT()
-        if temp_hum is not None:
-            info = {
-                "temperature": temp_hum[0],
-                "humidity": temp_hum[1],
-                }
-            info_s = json.dumps(info)
-            # print('"temperature":{} '.format(info_s))
+def startLooping():
+    log_file.write("startLooping\n")
+    try:
+        # Initialize our MQTTClient and connect to the MQTT server
+        mqtt_client = MQTTClient(
+                client_id=mqtt_client_id,
+                server=mqtt_host,
+                port=mqtt_port)
+        mqtt_client.connect()
+        while True:
+            temp_hum = readDHT()
+            if isinstance(temp_hum, OSError):
+                log_str = "e1：%s" %str(temp_hum)
+                log_file.write(log_str + "\n")
+                log_file.flush()
+            elif temp_hum is not None:
+                try:
+                    info = {
+                        "temperature": temp_hum[0],
+                        "humidity": temp_hum[1],
+                        }
+                    info_s = json.dumps(info)
+                    # print('"temperature":{} '.format(info_s))
+                    log_str = "t：%s" %str(time.localtime())
+                    log_file.write(log_str + "\n")
+                    log_file.write(info_s + "\n")
+                    log_file.flush()
+                    mqtt_client.publish(mqtt_publish_topic, info_s)
+                except Exception as e:
+                    log_str = "e2：%s" %str(e)
+                    log_file.write(log_str + "\n")
+                    log_file.flush()
+            else:
+                # print('the value is none')
+                pass
             
-            mqtt_client.publish(mqtt_publish_topic, info_s)
-        else:
-            # print('the value is none')
-            pass
+            # Delay a bit to avoid hitting the rate limit
+            log_file.write("before sleep\n")
+            log_file.flush()
+            time.sleep(120)
+            log_file.write("after sleep\n")
+            log_file.flush()
+    except SystemExit as e:
+        log_file.write("SystemExit\n")
+    except KeyboardInterrupt as e:
+        log_file.write("KeyboardInterrupt\n")
+    except GeneratorExit as e:
+        log_file.write("GeneratorExit\n")
+    except Exception as e:
+        log_str2 = "e4：%s" %str(e)
+        log_file.write(type(e) + "\n")
+        log_file.write(log_str2 + "\n")
+        log_file.flush()
+        # print(f'Failed to publish message: {e}')
+    finally:
+        mqtt_client.disconnect()
+        log_file.write("disconnected mqtt\n")
+        log_file.flush()
+        time.sleep(6)
+        startLooping()
+        # machine.reset()
         
-        # Delay a bit to avoid hitting the rate limit
-        time.sleep(120)
-except Exception as e:
-    print(f'Failed to publish message: {e}')
-finally:
-    mqtt_client.disconnect()
+startLooping()
+
+
--- main.py
+++ main.py
@@ -18,21 +18,6 @@ wifi_password = "******"
 
 # 日志相关
 log_file = open("_log.txt", "w")
-startTime_str = "the start time：%s" %str(time.localtime())
-log_file.write(startTime_str + "\n")
-log_file.flush()
-
-# Connect to WiFi
-wlan = network.WLAN(network.STA_IF)
-wlan.config(pm=network.WLAN.PM_NONE)
-wlan.active(True)
-wlan.connect(wifi_ssid, wifi_password)
-while wlan.isconnected() == False:
-    print('Waiting for connection...')
-    time.sleep(1)
-print("Connected to WiFi")
-
-led_pin.on()
 
 # Fill in your Adafruit IO Authentication and Feed MQTT Topic details
 mqtt_host = "192.168.31.11"
@@ -43,53 +28,78 @@ mqtt_publish_topic = "/fridge/temp-humidity"  # The MQTT topic for your Adafruit
 # It needs to be globally unique across all of Adafruit IO.
 mqtt_client_id = "WCwsVCBZa1xcSlRTUzwsaXkiUXlwOVVgKg"
 
+# Connect to WiFi
+wlan = network.WLAN(network.STA_IF)
+wlan.config(pm=network.WLAN.PM_NONE)
+wlan.active(True)
+wlan.connect(wifi_ssid, wifi_password)
+while wlan.isconnected() == False:
+    print('Waiting for connection...')
+    log_file.write("Waiting for connection...\n")
+    time.sleep(1)
+print("Connected to WiFi")
+log_file.write("Connected to WiFi\n")
+
+led_pin.on()
+
 # Initialize our MQTTClient and connect to the MQTT server
 mqtt_client = MQTTClient(
         client_id=mqtt_client_id,
         server=mqtt_host,
         port=mqtt_port)
-
 mqtt_client.connect()
+log_file.write("Connected to MQTT\n")
 
-
-counter = 0
-try:
-    while True:
-        temp_hum = readDHT()
-        if isinstance(temp_hum, OSError):
-            log_str = "e1：%s" %str(temp_hum)
-            log_file.write(log_str + "\n")
-            log_file.flush()
-        elif temp_hum is not None:
-            try:
+def startLooping():
+    log_file.write("startLooping\n")
+    try:
+        while True:
+            log_file.write("start readDHT\n")
+            temp_hum = readDHT()
+            log_file.write(str(type(temp_hum)))
+            log_file.write(" end readDHT\n")
+            if isinstance(temp_hum, Exception):
+                log_str = "e1：%s" %str(temp_hum)
+                log_file.write(log_str + "\n")
+                log_file.flush()
+            elif temp_hum is not None:
                 info = {
                     "temperature": temp_hum[0],
                     "humidity": temp_hum[1],
                     }
                 info_s = json.dumps(info)
                 # print('"temperature":{} '.format(info_s))
-                log_str = "t：%s" %str(time.localtime())
-                log_file.write(log_str + "\n")
                 log_file.flush()
                 mqtt_client.publish(mqtt_publish_topic, info_s)
-            except Exception as e:
-                log_str = "e2：%s" %str(e)
-                log_file.write(log_str + "\n")
-                log_file.flush()
-        else:
-            # print('the value is none')
-            pass
+            else:
+                # print('the value is none')
+                pass
+            
+            # Delay a bit to avoid hitting the rate limit
+            time.sleep(120)
+            #for x in range(0, 12):
+            #   time.sleep(10)
+            #   mqtt_client.ping()
+                
+    except SystemExit as e:
+        log_file.write("SystemExit\n")
+    except KeyboardInterrupt as e:
+        log_file.write("KeyboardInterrupt\n")
+    except GeneratorExit as e:
+        log_file.write("GeneratorExit\n")
+    except Exception as e:
+        log_str2 = "e4：%s" %str(e)
+        log_file.write(type(e) + "\n")
+        log_file.write(log_str2 + "\n")
+        log_file.flush()
+        # print(f'Failed to publish message: {e}')
+    finally:
+        mqtt_client.disconnect()
+        log_file.write("mqtt disconnected\n")
+        log_file.flush()
+        time.sleep(30)
+        startLooping()
+        # machine.reset()
         
-        # Delay a bit to avoid hitting the rate limit
-        time.sleep(120)
-except Exception as e:
-    log_str = "e4：%s" %str(e)
-    log_file.write(log_str + "\n")
-    log_file.flush()
-    # print(f'Failed to publish message: {e}')
-finally:
-    mqtt_client.disconnect()
-    log_file.write("disconnected mqtt")
-    log_file.flush()
-
+startLooping()
 
